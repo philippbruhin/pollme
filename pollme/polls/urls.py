3b7a8cda5b8c20e72d93e5bf4c38bc384aca087e
philippbruhin <info@philippbruhin.ch>
2019-01-16 23:47:54 +0100

    poll detail page

--- pollme/polls/urls.py
+++ pollme/polls/urls.py
@@ -5,4 +5,6 @@ app_name="polls"
 
 urlpatterns = [
     path('list/', views.polls_list, name='list'),
+    # polls/details/1/
+    path('details/<int:poll_id>/', views.poll_detail, name='detail')
 ]
--- pollme/polls/views.py
+++ pollme/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Poll
 
 def polls_list(request):
@@ -9,3 +9,12 @@ def polls_list(request):
     polls = Poll.objects.all()
     context = {'polls': polls}
     return render(request, 'polls/polls_list.html', context)
+
+def poll_detail(request, poll_id):
+    """
+    Renders the poll_detail.html template which allows a user to vote
+    on the choices of a poll.
+    """
+    poll = get_object_or_404(Poll, id=poll_id)
+    context = { 'poll': poll }
+    return render(request, 'polls/poll_detail.html', context)
