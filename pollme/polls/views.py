f20032fe3498fc93508b85f18182a50c65a8c1b4
philippbruhin <info@philippbruhin.ch>
2019-01-17 23:43:32 +0100

    choice error and summary page

--- pollme/polls/views.py
+++ pollme/polls/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from .models import Poll
+
+from .models import Choice, Poll
 
 def polls_list(request):
     """
@@ -15,6 +18,20 @@ def poll_detail(request, poll_id):
     Renders the poll_detail.html template which allows a user to vote
     on the choices of a poll.
     """
+    # poll = Poll.objects.get(id=poll_id)
     poll = get_object_or_404(Poll, id=poll_id)
+
     context = { 'poll': poll }
     return render(request, 'polls/poll_detail.html', context)
+
+def poll_vote(request, poll_id):
+    # try:
+    poll = get_object_or_404(Poll, id=poll_id)
+    choice_id = request.POST.get('choice')
+    if choice_id:
+        choice = Choice.objects.get(id=choice_id)
+        choice.votes += 1
+        choice.save()
+    else:
+        messages.error(request, 'No choice was found.')
+    return render(request, 'polls/poll_results.html', {'poll': poll})
