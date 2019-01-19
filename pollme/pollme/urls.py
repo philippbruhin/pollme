a25f93485729085e8a1390e49a60df5efb102799
philippbruhin <info@philippbruhin.ch>
2019-01-19 18:00:48 +0100

    added default login page

--- pollme/pollme/urls.py
+++ pollme/pollme/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('polls/', include('polls.urls', namespace='polls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
