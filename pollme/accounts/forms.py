fa1c2337105650ba3544b16f12d6279cae5857e7
philippbruhin <info@philippbruhin.ch>
2019-01-20 21:10:25 +0100

    registration validation

---
+++ pollme/accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100, min_length=5)
+    email = forms.EmailField(label='E-Mail')
+    password1 = forms.CharField(label='Password', max_length=100, min_length=5)
+    password2 = forms.CharField(label='Confirm Password', max_length=100, min_length=5)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise ValidationError('Emails is already registred.')
+        return email
--- pollme/accounts/views.py
+++ pollme/accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
+from .forms import UserRegistrationForm
 
 def login_user(request):
     if request.method == "POST":
@@ -20,4 +23,17 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
-    
+
+def user_registration(request):
+    if request.method =='POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+            user = User.objects.create_user(username, email=email, password=password)
+            messages.success(request, 'Thanks for registering {}'.format(user.username))
+            return HttpResponseRedirect(reverse('accounts:login'))
+    else:
+            form = UserRegistrationForm() 
+    return render(request, 'accounts/register.html', {'form': form})
