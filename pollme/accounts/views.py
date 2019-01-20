2de468a3e446e7964d39deb3e10f6468674e1f3b
philippbruhin <info@philippbruhin.ch>
2019-01-20 16:05:31 +0100

    added login and logout logic

--- pollme/accounts/views.py
+++ pollme/accounts/views.py
@@ -1,4 +1,23 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-def login(request):
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            messages.error(request, 'Bad username or password')
     return render(request, 'accounts/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+    
