5107eb0fd513ac6a12bf62d8aa2345f21d425890
philippbruhin <info@philippbruhin.ch>
2019-01-27 13:00:20 +0100

    check if voted more than once

--- pollme/polls/models.py
+++ pollme/polls/models.py
@@ -9,9 +9,24 @@ class Poll(models.Model):
     def __str__(self):
         return self.text
 
+    def user_can_vote(self, user):
+        """
+        Returns False if user has already voted, else True
+        """
+        user_votes = user.vote_set.all()
+        qs = user_votes.filter(poll=self)
+        if qs.exists():
+            return False
+        return True
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=255)
 
     def __str__(self):
         return "{} - {}".format(self.poll.text[:25], self.choice_text[:25])
+
+class Vote(models.Model):
+    user = models. ForeignKey(User, on_delete=models.CASCADE)
+    poll = models. ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
